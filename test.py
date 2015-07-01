ba5899db098afb45975b0ad4a4dbc3a6890d8a38
piashishi <piashishi@qq.com>
2015-07-01 16:04:23 +0800

    add parse in/out message

--- test.py
+++ test.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python 
 import re
+import os
 
 import private_data
 
+
+msgDirection = ""    #IPC_IN for incoming messages, IPC_OUT for out messages
+fragmentFlag = "" #  1/1 mean no fragment, 1/3 mean the first one of 3 fragments
+
+ALLProcessPairMessagesArray=[]  #collect all out message, each node is messageItem
+
+process_pair = [["TRACE_CTRL", "TRACE_PROXY"], ["TRACE_PROXY", "SC"]]
+
 def getNodeName(node, num):
-    if node == 0:
+    if node == 0:    #0 mean AS_NODE
         return private_data.as_map[num]
-    if node == 2:
+    if node == 2:    #2 mean CLA NODE
         return private_data.cla_map[num]
+    if node == 4:      #4 SAB
+        return private_data.sab_map[num]
     
 def getServerName(server, num):
     return "CLA?"
@@ -18,43 +29,105 @@ def getNGName(node, node_num, server, ser_num):
     else:
         return getNodeName(node, node_num)
    
+class messageItem:
+    def __init__(self, timestamp, msgType, msgData):
+        self.timestamp = timestamp
+        self.msgType = msgType
+        self.msgData = msgData
+        self.isReceived = False
+        
+    def setMsgReceived(self):
+        self.isReceived = True
     
-process=[]
-msg_direction = ""
+#processMessageList, store out messages for one process pair
+class processPairMessageList:
+    def __init__(self, srcNode,src, srcInstance, dstNode, dst, dstInstance, srcPid):
+        self.src = src
+        self.srcInstance = srcInstance
+        self.srcPid = srcPid
+        self.dst = dst
+        self.dstInstance = dstInstance
+        self.srcNode = srcNode
+        self.dstNode = dstNode
+        self.outMsg = []
+    def appendOutMsg(self, data):
+        self.outMsg.append(data)
+  
+def FindMessageItem(src, srcInstance, dst, dstInstance, senderPid):
+    for tmp in ALLProcessPairMessagesArray:
+        if tmp.src ==src and tmp.dst == dst and tmp.srcInstance == srcInstance \
+        and tmp.dstInstance ==dstInstance and tmp.srcPid == senderPid:
+            return tmp
+    return None
 
-process_pair = [["TRACE_CTRL", "TRACE_PROXY"], ["TRACE_PROXY", "SC"]]
 
-src_process = ["TRACE_CTRL", "TRACE_PROXY"]
-dst_process = ["TRACE_PROXY", "SC"]
+def appendFragmentMsg(line):
+    return
 
-#not finished
-def isTrace(src, dst):
+def isNeedTrace(src, dst):
     for pair in process_pair:
-        if pair[0] == src and pair[1] == dst:
+        if (pair[0] == src and pair[1] == dst) or (pair[0] == dst and pair[1] == src):
             return True
     return False
             
+def collectMsg(fileName, direction):            
+    fp = open(fileName, "r");
+    for line in fp.readlines():
+        msgLine = re.search("LIBMSG: MMON", line)
+        if msgLine:     
+            tmpArr = re.split(r' +', line);
+            msgInfo = re.split(r";", tmpArr[8])  # tmpArr[8] is LIB MSG information
+            fragmentFlag = msgInfo[2]  # get fragment flag
+            msgDirection = msgInfo[3]  # get msg direction
+           
+            matchObj = re.search(r'([0-9A-Fa-f]{2} ){2,}', line);
+            if matchObj:
+                msgData = re.split(r' ', matchObj.group())
+                
+                if fragmentFlag != "1/1":
+                    appendFragmentMsg(msgData)
+                    continue
 
-fp = open("raw.log", "r");
-for line in fp.readlines():
-    matchObj =  re.search("LIBMSG: MMON", line)
-    if matchObj:     
-        tmpArr = re.split(r' +', line);
-        arr2 = re.split(r";", tmpArr[8])    #tmpArr[8] is LIB MSG information
-        if arr2[2] != "1/1": #no fragment
-            continue
-        msg_direction = arr2[3]        #IPC_IN or IPC_OUT
-        if not tmpArr[5] in process:
-            process.append(tmpArr[5])
-        matchObj = re.search(r'([0-9A-Fa-f]{2} ){2,}', line);
+                srcProcess = private_data.process_map[int(msgData[22], 16)]
+                srcInstance = int(msgData[23], 16)
+                srcNode = getNGName(int(msgData[24], 16), int(msgData[25], 16), int(msgData[26], 16), int(msgData[27], 16))
+                dstProcess = private_data.process_map[int(msgData[28], 16)]
+                dstInstance = int(msgData[29], 16)
+                dstNode = getNGName(int(msgData[30], 16), int(msgData[31], 16), int(msgData[32], 16), int(msgData[33], 16))
+                srcPid = int(msgData[36] + msgData[35], 16) 
+            
+                if not isNeedTrace(srcProcess, dstProcess):
+                    continue;
+         
+                if msgDirection == direction and direction == "IPC_OUT":
+                    item = FindMessageItem(srcProcess, srcInstance, dstProcess, dstInstance, srcPid)
+                    if not item:
+                        item = processPairMessageList(srcNode, srcProcess, srcInstance, \
+                                        dstNode, dstProcess, dstInstance, srcPid)
+                        ALLProcessPairMessagesArray.append(item)
+                    item.appendOutMsg(msgData)
+                if msgDirection == direction and direction == "IPC_IN":
+                    print "IN"
+                
+    fp.close()
+
+for root,dirs,files in os.walk(r'./'):
+    for logFile in files:
+        matchObj = re.search(r"log$", logFile)    
         if matchObj:
-            tmpArr = re.split(r' ',matchObj.group())
-            if len(tmpArr) > 31 and msg_direction == "IPC_IN" :
-                print "SRC:",
-                print getNGName(int(tmpArr[24], 16), int(tmpArr[25], 16),int(tmpArr[26], 16),int(tmpArr[27], 16)), private_data.process_map[int(tmpArr[22], 16)], tmpArr[23],
-                print "----->",  
-                print "DST:",
-                print getNGName(int(tmpArr[30], 16), int(tmpArr[31], 16), int(tmpArr[32], 16),int(tmpArr[33], 16)), private_data.process_map[int(tmpArr[28], 16)], tmpArr[29]
-fp.close()
-print process
+            collectMsg(logFile, "IPC_OUT")
+
+for root,dirs,files in os.walk(r'./'):
+    for logFile in files:
+        matchObj = re.search(r"log$", logFile)    
+        if matchObj:
+            collectMsg(logFile, "IPC_IN")
+         
+for tmp in ALLProcessPairMessagesArray:
+    print tmp.srcNode+"/"+tmp.src+"_"+str(tmp.srcInstance)+"["+str(tmp.srcPid)+"]"+"------->"+tmp.dstNode+ \
+    "/"+tmp.dst+"_"+str(tmp.dstInstance)
+    for tmp2 in tmp.outMsg:
+        print tmp2
+    
+
             
